67a7278944c86ff04ca42e6fc88299fbd7b11aed
Hyungmo Gu <guhyungm7@gmail.com>
2017-01-01 14:34:28 -0800

    configure database

---
+++ databse_setup.py
@@ -0,0 +1,11 @@
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String, Float
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+
+engine = create_engine('sqlite:///itemcatalogue.db')
