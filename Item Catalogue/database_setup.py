04f46b619cc98c214938a5fc44de2541108d3ba4
Hyungmo Gu <guhyungm7@gmail.com>
2017-01-07 15:43:59 -0800

    Fix database columns

--- Item Catalogue/database_setup.py
+++ Item Catalogue/database_setup.py
@@ -9,14 +9,15 @@ Base = declarative_base()
 # create database equivalent of tables
 class Category(Base):
 	__tablename__ = 'category'
-	name = Column(String(80), primary_key=True)
+	id = Column(Integer, primary_key = True)
+	name = Column(String(80), nullable= False)
 
 class MenuItem(Base):
 	__tablename__ = 'menu_item'
-	name = Column(String(80), primary_key=True)
+	id = Column(Integer, primary_key = True)
+	name = Column(String(80), nullable=False)
 	description = Column(String(300))
-	date_created = Column(DateTime,nullable=False)
-	category_name = Column(String(80),ForeignKey('category.name'))
+	category_name = Column(String(80),ForeignKey('category.id'))
 	category = relationship(Category)
 
 # put everything together in a file called itemcatalogue.db
