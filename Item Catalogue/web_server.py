0fe6999c1895c230f80793e58711d9a9c8917641
Hyungmo Gu <guhyungm7@gmail.com>
2017-01-07 19:24:59 -0800

    Fix the issue of non-refreshing css file

--- Item Catalogue/web_server.py
+++ Item Catalogue/web_server.py
@@ -1,6 +1,8 @@
+import os
+
 from datetime import datetime
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, desc, asc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Category, MenuItem
 
@@ -12,18 +14,18 @@ DBSession = sessionmaker(bind = engine)
 
 @app.route('/')
 def readMainPage():
-"""
-readMainPage()
+	"""
+	readMainPage()
 
-Returns category and 20 most recent menu items
-"""
+	Returns category and 20 most recent menu items
+	"""
 	session = DBSession()
 
 	#query all entries in category. Store result in a variable called categories
-	categories = session.query(Category).all()
+	categories = session.query(Category).order_by(asc(Category.name)).all()
 
 	#query 20 most recent entries in menu item. store result in a variable called menuItems.
-	menuItems = session.query(MenuItem).order_by(desc(MenuItem.date_created)).limit(20).all()
+	menuItems = session.query(MenuItem).order_by(desc(MenuItem.id)).limit(20).all()
 
 	session.close()
 	#return template filled with query result
@@ -31,13 +33,12 @@ Returns category and 20 most recent menu items
 
 # @app.route('/<string:category_name>/')
 # def readCategory(category_name):
-# """
-# readCategory()
+# 	"""
+# 	readCategory()
 
-# Returns all categories and menu items of whatever clicked category in alphabetical order
+# 	Returns all categories and menu items of whatever clicked category in alphabetical order
 
-# """	
-
+# 	"""	
 # 	session = DBSession()
 
 # 	#query all entries in category.
@@ -45,9 +46,12 @@ Returns category and 20 most recent menu items
 
 # 	#query all menu items in the category
 # 	menuItems = session.query(MenuItem).filter_by(category_name= category_name).order_by(desc(MenuItem.name))
+	
+# 	count = session.query(func.count(MenuItem)).filter_by(category_name=category_name).group_by(MenuItem.category_name)
+
 # 	session.close()
 
-# 	return render_template('category.html')
+# 	return render_template('category.html',category_name=category_name,categories=categories,menuItems=menuItems,count=count)
 
 
 # @app.route('/<string:category_name>/new', methods=['GET','POST'])
@@ -97,6 +101,20 @@ Returns category and 20 most recent menu items
 # def readMenuItem(category_name,item_name):
 
 # 	return render_template('menuItem.html')
+
+#for url_for cash busting. This solves the trouble of css not refreshing the old file. More info here: http://flask.pocoo.org/snippets/40/
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                     endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
 
 app.debug = False
 app.run(host='0.0.0.0', port=5000)
