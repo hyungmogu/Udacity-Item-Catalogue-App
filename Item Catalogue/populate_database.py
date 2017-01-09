4320d490abb91467d5746089b144a3dcf583f4b2
Hyungmo Gu <guhyungm7@gmail.com>
2017-01-09 14:14:22 -0800

    Populate table with dummy data

--- Item Catalogue/populate_database.py
+++ Item Catalogue/populate_database.py
@@ -9,6 +9,10 @@ DBSession = sessionmaker(bind = engine)
 
 session = DBSession()
 
+#cleaning tables before populating
+session.query(Category).delete()
+session.query(MenuItem).delete()
+session.commit()
 
 #adding categories
 category_names = ['Soccer','Basketball','Frisbee','Snowboarding']
@@ -20,15 +24,18 @@ for category_name in category_names:
 
 
 #adding menu items for soccer category
-shingGuards = MenuItem(name="Two Shinguards",category_name="Soccer",date_created= datetime.now())
+soccer = session.query(Category).filter_by(name="Soccer").first()
+shingGuards = MenuItem(name="Two Shinguards", category_id=soccer.id)
 session.add(shingGuards)
 session.commit()
 
 #adding menu items for snowboarding category
 sbItemNames = ["Goggles","Snowboard"]
+snowboarding = session.query(Category).filter_by(name="Snowboarding").first()
+
 
 for sbItemName in sbItemNames:
-	newSbItem = MenuItem(name = sbItemName, category_name = "Snowboarding", date_created= datetime.now())
+	newSbItem = MenuItem(name = sbItemName, category_id=snowboarding.id)
 	session.add(newSbItem)
 	session.commit()
 
