0a9d4eb420c1d49d9e562d22e32076b6827bb609
Hyungmo Gu <guhyungm7@gmail.com>
2017-02-13 00:07:51 -0800

    Fix(Vagrantfile): Re-initialize Vagrant
    The old vagrant doesn't have sqlalchemy and flask installed; git won't recognize changes made in Vagrantfile. I tried to make changes using 'vagrant package', but the file size is too big (>700MB). I decided to go another way is by manually making git to recognize the changes by deleting and then re-adding with vagrant with correct install. This is what I am doing.

--- pg_config.sh
+++ pg_config.sh
@@ -1,6 +1,6 @@
 apt-get -qqy update
+apt-get -qqy install sqlite3
 apt-get -qqy install postgresql python-psycopg2
-apt-get -qqy install python-flask python-sqlalchemy
 apt-get -qqy install python-pip
 pip install bleach
 pip install oauth2client
@@ -9,7 +9,10 @@ pip install httplib2
 pip install redis
 pip install passlib
 pip install itsdangerous
+pip install flask
 pip install flask-httpauth
+pip install sqlalchemy
+
 su postgres -c 'createuser -dRS vagrant'
 su vagrant -c 'createdb'
 su vagrant -c 'createdb forum'
