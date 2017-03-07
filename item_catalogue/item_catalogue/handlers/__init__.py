9539ad34515133aa28d749b73cfa055b08b40aea
Hyungmo Gu <guhyungm7@gmail.com>
2017-03-06 20:32:17 -0800

    refactor: Move codes to handlers/__init__.py
    Motivation:
      - Took small step towards refactoring the code.

--- item_catalogue/item_catalogue/handlers/__init__.py
+++ item_catalogue/item_catalogue/handlers/__init__.py
@@ -1,3 +1,20 @@
+import os
+import string
+import random
+
+import httplib2
+import json
+import requests
+from flask import render_template, request, url_for, redirect, flash, jsonify, make_response
+from flask import session as login_session
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+from sqlalchemy import asc, desc, exc
+
+from item_catalogue import app, DBSession, CLIENT_ID
+from item_catalogue.model.category import Category
+from item_catalogue.model.menuitem import MenuItem
+
 #EXTRA FUNCTIONS
 def is_signed_in():
 	if "username" not in login_session:
@@ -509,23 +526,3 @@ def readWelcome():
 
 	return render_template("welcome.html",username=login_session["username"])
 
-
-# url_for cash busting. 
-# Note: This solves the trouble of css not refreshing the old file. 
-# More info can be found here: http://flask.pocoo.org/snippets/40/ .
-@app.context_processor
-def override_url_for():
-    return dict(url_for=dated_url_for)
-
-def dated_url_for(endpoint, **values):
-    if endpoint == "static":
-        filename = values.get("filename", None)
-        if filename:
-            file_path = os.path.join(app.root_path,
-                                     endpoint, filename)
-            values['q'] = int(os.stat(file_path).st_mtime)
-    return url_for(endpoint, **values)
-
-app.debug = False
-app.secret_key = 'A9012ASD09812@)(J*AS(&FJHASHVUaiuw1bSA&Dy712bhc'
-app.run(host='0.0.0.0', port=5000)
