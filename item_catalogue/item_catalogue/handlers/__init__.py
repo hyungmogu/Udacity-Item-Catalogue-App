1e010b71e9ea3cf057c9edb7f3c7c964387e10ea
Hyungmo Gu <guhyungm7@gmail.com>
2017-03-07 21:55:23 -0800

    refactor: Reorganize welcome page handler
    Summary
      - Separated the page handler from /handlers/__init__.py
        using 'blueprints'
    
    Motivation behind this commit:
      - Smooth transitioning by making incremantal changes
        - Making troubleshooting manageable if it occurs
        - Minimizing the feeling of being overwhelmed
    
    Goal:
      - Making codes in item_catalogue readible and maintainable

--- item_catalogue/item_catalogue/handlers/__init__.py
+++ item_catalogue/item_catalogue/handlers/__init__.py
@@ -1,8 +1,6 @@
-from flask import render_template, url_for, redirect, flash
-from flask import session as login_session
-from sqlalchemy import asc, desc, exc
+from flask import url_for, redirect, flash
 
-from item_catalogue import app, DBSession
+from item_catalogue import app
 from item_catalogue.model.category import Category
 from item_catalogue.model.menuitem import MenuItem
 from item_catalogue.handlers.login.routes import mod
@@ -11,6 +9,7 @@ from item_catalogue.handlers.post.routes import mod
 from item_catalogue.handlers.api.routes import mod
 from item_catalogue.handlers.home.routes import mod
 from item_catalogue.handlers.category.routes import mod
+from item_catalogue.handlers.welcome.routes import mod
 from . import helper
 
 app.register_blueprint(login.routes.mod)
@@ -19,19 +18,10 @@ app.register_blueprint(post.routes.mod)
 app.register_blueprint(api.routes.mod)
 app.register_blueprint(home.routes.mod)
 app.register_blueprint(category.routes.mod)
+app.register_blueprint(welcome.routes.mod)
 
 #ERROR HANDLERS
 @app.errorhandler(404)
 def page_not_found(error):
 	flash("Not allowed. The page or item you've requested doesn't exist.", "error")
 	return redirect(url_for("home.readMain"))
-
-#ROUTES
-@app.route("/welcome/")
-def readWelcome():
-	if not helper.is_signed_in():
-		flash("Not allowed. 'Welcome' page requires login.", "error")
-		redirect(url_for("login.readLogin"))
-
-	return render_template("welcome.html",username=login_session["username"])
-
