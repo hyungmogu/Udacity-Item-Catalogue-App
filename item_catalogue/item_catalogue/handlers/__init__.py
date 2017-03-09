9039c52b027c16bb0e273655b403ca8605d051b0
Hyungmo Gu <guhyungm7@gmail.com>
2017-03-09 10:11:07 -0800

    refactor: Relocate error handlers
    Summary:
      - Moved error handler to handlers/error/routes.py
    
    Goal:
      - Making codes in item_catalogue more readible and maintainable.

--- item_catalogue/item_catalogue/handlers/__init__.py
+++ item_catalogue/item_catalogue/handlers/__init__.py
@@ -10,6 +10,7 @@ from item_catalogue.handlers.api.routes import mod
 from item_catalogue.handlers.home.routes import mod
 from item_catalogue.handlers.category.routes import mod
 from item_catalogue.handlers.welcome.routes import mod
+from item_catalogue.handlers.error.routes import mod
 from . import helper
 
 app.register_blueprint(login.routes.mod)
@@ -19,9 +20,5 @@ app.register_blueprint(api.routes.mod)
 app.register_blueprint(home.routes.mod)
 app.register_blueprint(category.routes.mod)
 app.register_blueprint(welcome.routes.mod)
+app.register_blueprint(error.routes.mod)
 
-#ERROR HANDLERS
-@app.errorhandler(404)
-def page_not_found(error):
-	flash("Not allowed. The page or item you've requested doesn't exist.", "error")
-	return redirect(url_for("home.readMain"))
